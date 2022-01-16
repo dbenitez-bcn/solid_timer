46ac10b69130938865802da36fa29dbbdbcf74bd
David Benitez <dbr.bcn@gmail.com>
2022-01-16 19:19:41 +0100

    cleans main

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,12 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
