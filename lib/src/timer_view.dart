f644b66856021e462214334c03b8031bd680f914
David Benitez <dbr.bcn@gmail.com>
2022-01-16 19:19:28 +0100

    adds animation

--- lib/src/timer_view.dart
+++ lib/src/timer_view.dart
@@ -7,32 +7,56 @@ class TimerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      width: MediaQuery.of(context).size.width - 24.0,
-      height: MediaQuery.of(context).size.width - 24.0,
-      color: Colors.black,
-      child: const TimerProgressIndicator(
-        percentage: 1,
+    return const Center(
+      child: SizedBox.expand(
+        child: TimerProgressIndicator(
+          duration: 1,
+        ),
       ),
-    ));
+    );
   }
 }
 
-class TimerProgressIndicator extends StatelessWidget {
-  final double percentage;
+class TimerProgressIndicator extends StatefulWidget {
+  final int duration;
 
-  const TimerProgressIndicator({Key? key, required this.percentage})
-      : assert(percentage >= 0),
+  const TimerProgressIndicator({Key? key, required this.duration})
+      : assert(duration >= 0),
         super(key: key);
 
   @override
+  State<TimerProgressIndicator> createState() => _TimerProgressIndicatorState();
+}
+
+class _TimerProgressIndicatorState extends State<TimerProgressIndicator>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: widget.duration),
+    )..repeat();
+    // _animation = Tween<double>(begin: 2.0, end: 3.0)
+    _animation = Tween<double>(begin: 0.0, end: 1)
+        .animate(_controller);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: TimerPainter(
-        percentage,
-        color: Theme.of(context).primaryColor,
-      ),
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return CustomPaint(
+          painter: TimerPainter(
+            percentage: _animation.value,
+            color: Theme.of(context).primaryColor,
+          ),
+        );
+      },
     );
   }
 }
@@ -41,15 +65,15 @@ class TimerPainter extends CustomPainter {
   final double percentage;
   final Color color;
 
-  TimerPainter(this.percentage, {required this.color});
+  TimerPainter({required this.percentage, required this.color});
 
   @override
   void paint(Canvas canvas, Size size) {
-    _drawCircle(size, canvas);
-    _drawArc(size, canvas);
+    _drawOutline(size, canvas);
+    _drawProgress(size, canvas);
   }
 
-  void _drawArc(Size size, Canvas canvas) {
+  void _drawProgress(Size size, Canvas canvas) {
     final center = Offset(size.width / 2, size.height / 2);
     final radius = size.width / 2;
     final rect = Rect.fromCircle(center: center, radius: radius);
@@ -62,7 +86,7 @@ class TimerPainter extends CustomPainter {
     canvas.drawArc(rect, start, end, false, paint);
   }
 
-  void _drawCircle(Size size, Canvas canvas) {
+  void _drawOutline(Size size, Canvas canvas) {
     final center = Offset(size.width / 2, size.height / 2);
     final radius = size.width / 2;
     final paint = Paint()
