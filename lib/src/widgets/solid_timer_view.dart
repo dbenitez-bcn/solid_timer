7e6162c923f607f8defc8812bb430b7b1e04f892
David Benitez <dbr.bcn@gmail.com>
2022-01-22 16:22:58 +0100

    adds delay loader

--- lib/src/widgets/solid_timer_view.dart
+++ lib/src/widgets/solid_timer_view.dart
@@ -6,20 +6,35 @@ import 'package:solid_timer/src/widgets/timer_view.dart';
 class SolidTimer extends StatelessWidget {
   const SolidTimer({Key? key}) : super(key: key);
 
+  Future<void> foo() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SolidTimerBloc(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            child: TimerView(),
-          ),
-          ControlButtons()
-        ],
-      ),
+    return FutureBuilder(
+      future: foo(),
+      builder: (context, snapshot) {
+        print("SNAPSHOT STATE=> ${snapshot.connectionState}");
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            return SolidTimerBloc(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    child: const TimerView(),
+                  ),
+                  const ControlButtons()
+                ],
+              ),
+            );
+          default:
+            return const Center(child: CircularProgressIndicator.adaptive());
+        }
+      },
     );
   }
 }
