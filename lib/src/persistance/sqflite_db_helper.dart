e604b46dd033ea787b00e8903f27d8121987a375
David Benitez <dbr.bcn@gmail.com>
2022-02-12 11:29:28 +0100

    adds last selected timer table

--- lib/src/persistance/sqflite_db_helper.dart
+++ lib/src/persistance/sqflite_db_helper.dart
@@ -5,16 +5,22 @@ import 'package:sqflite/sqflite.dart';
 const List<String> _migrations = [
   '''
   CREATE TABLE timers (id INTEGER PRIMARY KEY, seconds INTEGER NOT NULL)
+  ''',
+  '''
+  CREATE TABLE last_selected_timer (id INTEGER PRIMARY KEY, seconds INTEGER NOT NULL)
   '''
 ];
 const List<String> _downgrades = [
   '''
   DROP TABLE IF EXISTS timers
+  ''',
+  '''
+  DROP TABLE IF EXISTS last_selected_timer
   '''
 ];
 
 Future<Database> dbInitialization() async {
-  return await openDatabase("solid_timer.db", version: 1, onCreate: _onCreate, onUpgrade: _onUpdate, onDowngrade: _onDowngrade);
+  return await openDatabase("solid_timer.db", version: 2, onCreate: _onCreate, onUpgrade: _onUpdate, onDowngrade: _onDowngrade);
 }
 
 Future<void> _onCreate(Database db, int version) async {
